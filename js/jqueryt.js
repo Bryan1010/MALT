b2469bba7094e7937d822a3dae33dbf3a2ee6b4d
bryan1010 <Bryan.c@ens-labs.com>
2015-06-01 20:07:46 -0400

    Organized stuff

--- js/jqueryt.js
+++ js/jqueryt.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $ ("#register").hide
-$("#signup").click(function(){
-    ("#register").slideDown();
+    $("#signup").click(function(){
+        ("#register").slideDown();
+    });
 });
